7c3f8db035063cfb7f3a903711763faed751ab26
pBread <4473570+pBread@users.noreply.github.com>
2024-05-30 13:21:57 -0600

    added env sid

--- src/index.ts
+++ src/index.ts
@@ -12,4 +12,28 @@ const {
 const client = twilio(TWILIO_API_KEY, TWILIO_API_SECRET, { accountSid });
 
 console.log("==script started==");
-(async () => {})();
+(async () => {
+  const envSid = await getEnvironmentSid(FUNCTION_SERVICE_SID);
+  console.log(`Environment SID: ${envSid}`);
+})();
+
+async function getEnvironmentSid(fnSvcSid: string) {
+  const environments = await client.serverless.v1
+    .services(fnSvcSid)
+    .environments.list();
+
+  if (environments.length !== 1) {
+    console.error(
+      `Expected one environment, receieved ${environments.length}`,
+      environments
+    );
+
+    throw Error(`Expected one environment, receieved ${environments.length}`);
+  }
+  const environmentSid = environments[0].sid;
+
+  if (!environmentSid)
+    throw Error(`Invalid Environment SID. Received: ${environmentSid}`);
+
+  return environmentSid;
+}
